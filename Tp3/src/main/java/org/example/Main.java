164d91a058ace10b9001f62846edcb5f9ed457b1
alexisyassuff <a.yassuff@alumno.um.edu.ar>
2025-04-28 15:41:50 -0300

    Test basicos

--- Tp3/src/main/java/org/example/Main.java
+++ Tp3/src/main/java/org/example/Main.java
@@ -1,8 +1,19 @@
 package org.example;
-
+import org.example.Enum.Estado;
+import org.example.Clases.Libro;
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-
+    @Test
+    public void testCrearLibroValido() {
+        Libro libro = new Libro("978-3-16-148410-0", "Clean Code", "Robert C. Martin");
+        assertEquals("978-3-16-148410-0", libro.getISBN());
+        assertEquals("Clean Code", libro.getTitulo());
+        assertEquals("Robert C. Martin", libro.getAutor());
+        assertEquals(Estado.DISPONIBLE, libro.getEstado());
+        libro.mostrarLibro();
+    }
 }
