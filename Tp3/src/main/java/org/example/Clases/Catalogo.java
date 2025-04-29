3f4179b7445c4863792babaceb0e6adc16e16d03
alexisyassuff <a.yassuff@alumno.um.edu.ar>
2025-04-28 21:00:37 -0300

    Creacion de pruebas unitarias mas complejas

--- Tp3/src/main/java/org/example/Clases/Catalogo.java
+++ Tp3/src/main/java/org/example/Clases/Catalogo.java
@@ -1,10 +1,13 @@
 package org.example.Clases;
+import org.example.Enum.Estado;
 
+import java.util.stream.Collectors;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Catalogo {
-    List<Libro> lista = new ArrayList<>();
+    private List<Libro> lista = new ArrayList<>();
 
     public void agregarLibro(Libro libro){
         lista.add(libro);
@@ -14,4 +17,17 @@ public class Catalogo {
         return new ArrayList<>(lista);  // Devolvés una copia
     }
 
-}
+    public Libro buscarPorISBN(String ISBN) {
+        return lista.stream()
+                .filter(r -> r.getISBN().equalsIgnoreCase(ISBN))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public List<Libro> obtenerLibrosDisponibles() {
+        return lista.stream()
+                .filter(r -> r.getEstado() == Estado.DISPONIBLE)
+                .collect(Collectors.toList());
+    }}
+
+
